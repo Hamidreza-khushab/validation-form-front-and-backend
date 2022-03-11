7395dd2d8078eeaad14e505beb86ce6d33e7d768
Hamidreza <qiisohamid@gmail.com>
2022-03-11 23:06:22 +0100

    server initial setting

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+// EXTERNAL DEPENDENIES
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+const cors = require('cors');
+
+// IMPORTS
+
+// VARIABLES
+
+const port = process.env.PORT || 8080;
+const app =express();
+
+// CONNECT TO DB
+
+mongoose
+    .connect('mongodb://localhost:27017/loginAndRegistration')
+    .then(() =>
+    {
+        console.log('connected to db');
+    })
+    .catch(err => {console.log('db not connected', err.message);});
+
+// REQUEST PARSERS
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// STATIC FILES
+
+// ROUTES
+// app.use('/users', usersRouter);
+
+/** ERROR HANDLING */
+app.use((req, res, next) => 
+{
+    const error = new Error('Looks like something broke...');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => 
+{
+    res.status(err.status || 500).send({ error:{ message: err.message } });
+});
+//  LISTENER 
+app.listen(port, () =>
+{
+    console.log(`server is listenig on ${port} ..`);
+});
