f904459139f1a8b3c6f3bb09c235bfc76ac8c521
Hamidreza <qiisohamid@gmail.com>
2022-03-14 22:57:37 +0100

    add front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Registration from './components/Registration'
+import Container from 'react-bootstrap/Container'
+function App() {
+  return (
+    <Container>
+      <Registration/>
+    </Container>
+  );
+}
+
+export default App;
---
+++ front/src/components/Registration.jsx
@@ -0,0 +1,61 @@
+import Form from 'react-bootstrap/Form'
+import  Button  from 'react-bootstrap/Button'
+import axios from 'axios'
+import {useState , useEffect} from 'react'
+function Registration() {
+    const [user , setUser] = useState({})
+    const [firstName , setFirstName] = useState();
+    const [lastName , setLastName] = useState();
+    const [email , setEmail] = useState();
+    const [password , setPassword] = useState();
+
+  const handelSaveUser = () =>
+  {
+    setUser({firstName: firstName ,lastName : lastName , email: email, password: password });
+    
+  }
+//   console.log(user);
+  return (
+    <>
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicfirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control 
+            type="text"
+            onChange={(e)=>setFirstName(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasiclastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control 
+            type="text"
+            onChange={(e)=>setLastName(e.target.value)}
+            />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control 
+            type="email"
+            onChange={(e)=>setEmail(e.target.value)}
+              />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+            type="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+        </Form.Group>
+
+  <Button 
+  variant="primary"
+  onClick={()=>handelSaveUser()}
+   
+   >
+    save
+  </Button>
+</Form>
+    </>
+  )
+}
+
+export default Registration
